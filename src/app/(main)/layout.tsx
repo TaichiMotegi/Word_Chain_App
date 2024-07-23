1dab5a72d55417cc74c029bd7addc55593819167
Taichi Motegi <>
2024-07-23 16:45:02 +0000

    💄 エラー画面の追加とヘッダーデザインの修正

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,21 +5,11 @@ const MainLayout = ({
 }>) => {
   return (
     <div>
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">しりとりゲーム</span>
+      <header>
+        <div className="mx-auto flex  p-5  justify-center">
+          <h1 className="text-center text-xl text-gray-600 font-bold">
+            しりとりゲーム
+          </h1>
         </div>
       </header>
       <div className="flex h-screen">
