1c4436530627c9922861f3c645b67f81b3779ef0
Taichi Motegi <>
2024-07-25 13:00:02 +0000

    💄 ホーム画面とヘッダーのスタイルを変更

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,12 +7,9 @@ const MainLayout = ({
 }>) => {
   return (
     <div>
-      <header>
+      <header className="bg-gray-700">
         <div className="mx-auto flex p-5 justify-center">
-          <Link
-            href="/"
-            className="text-center text-xl text-gray-600 font-bold"
-          >
+          <Link href="/" className="text-center text-xl text-white font-bold">
             <div>しりとりゲーム</div>
           </Link>
         </div>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,30 +3,33 @@ import Link from "next/link";
 export default function MainPage() {
   return (
     <div className="h-screen flex flex-col items-center">
-      <h1 className="text-3xl mt-5">
-        しりとりソロプレイ！ 制限時間内に何単語答えられるか挑戦しよう！
+      <h1 className="text-3xl mt-20">
+        しりとりソロプレイ！ 1回答5秒以内で何単語答えられるか挑戦しよう！
       </h1>
       <h2 className="text-3xl mt-10">ルール</h2>
       <div className="text-xl mt-5 bg-slate-200 px-8 py-2">
         <ol className="list-decimal">
-          <li className="py-3">んで終わる単語を使用したらGAME OVER</li>
-          <li className="py-3">一度使用した単語を使用したらGAME OVER</li>
+          <li className="py-3">5秒以内に答える</li>
+          <li className="py-3">ひらがなのみで入力する</li>
           <li className="py-3">
-            小文字で終わる場合はその文字から始まる単語を入力してください <br />
-            例：きんぎょ → ヨーロッパ
-          </li>
-          <li className="py-3">
-            伸ばし棒で終わる場合はその前の文字から始まる単語を入力してください{" "}
+            小文字で終わる場合はその文字から始まる単語を入力する
             <br />
-            例：アカデミー → みそ
+            例：きんぎょ → よーろっぱ
           </li>
+          <li className="py-3">
+            伸ばし棒で終わる場合はその前の文字から始まる単語を入力する
+            <br />
+            例：すきー → きなこ
+          </li>
+          <li className="py-3">んで終わったらGame Over</li>
+          <li className="py-3">一度使用した単語を使用したらGame Over</li>
         </ol>
       </div>
       <Link
         href="/game"
-        className="font-semibold text-xl text-center border mt-32 w-2/5 px-4 py-5 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-700"
+        className="font-semibold text-xl text-center border mt-24 w-1/4 px-4 py-5 rounded-md shadow-sm text-white bg-indigo-700 hover:bg-indigo-500"
       >
-        <div>スタート</div>
+        <div>Play Game</div>
       </Link>
     </div>
   );
