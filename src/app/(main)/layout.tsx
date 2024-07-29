937c434c29a6715bd1de0693a284008f9ae80560
Taichi Motegi <>
2024-07-29 18:08:37 +0000

    💄 レイアウトの変更

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ const MainLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <header className="bg-gray-700">
         <div className="mx-auto flex p-5 justify-center">
           <Link href="/" className="text-center text-xl text-white font-bold">
@@ -14,27 +14,30 @@ const MainLayout = ({
           </Link>
         </div>
       </header>
-      <div className="flex flex-col h-full">
-        <main>{children}</main>
-        <footer className="absolute inset-x-0 bottom-2 flex items-cente">
-          <div className="w-full flex justify-center items-center">
-            <span className="flex-1 text-end">©Word Chain App</span>
-            <span className="px-5 text-center"> | </span>
-            <span className="text-center">
-              <Link href="https://developer.yahoo.co.jp/sitemap/">
-                Web Services by Yahoo! JAPAN
-              </Link>
-            </span>
-            <span className="px-5 text-center"> | </span>
-            <span className="flex-1">
-              <Link href="https://www.mediawiki.org/wiki/API:Main_page/ja">
-                This application uses the MediaWiki API
-              </Link>
-            </span>
-          </div>
-        </footer>
+      <div className="flex-grow flex flex-col justify-center items-center">
+        <main className="w-full flex flex-col justify-center items-center">
+          {children}
+        </main>
       </div>
+      <footer className="w-full flex items-center justify-center p-4">
+        <div className="w-full flex justify-center items-center">
+          <span className="flex-1 text-end">©Word Chain App</span>
+          <span className="px-5 text-center"> | </span>
+          <span className="text-center">
+            <Link href="https://developer.yahoo.co.jp/sitemap/">
+              Web Services by Yahoo! JAPAN
+            </Link>
+          </span>
+          <span className="px-5 text-center"> | </span>
+          <span className="flex-1">
+            <Link href="https://www.mediawiki.org/wiki/API:Main_page/ja">
+              This application uses the MediaWiki API
+            </Link>
+          </span>
+        </div>
+      </footer>
     </div>
   );
 };
+
 export default MainLayout;
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,13 +7,13 @@ export default function MainPage() {
       <div className="text-xl mt-5 px-8 py-2 border-4 border-gray-400 rounded-xl">
         <ol className="list-decimal font-semibold">
           <li className="py-3">5秒以内に答える</li>
-          <li className="py-3">ひらがなのみで入力する</li>
+          <li className="py-3">ひらがなと長音符のみで回答する</li>
           <li className="py-3">
-            小文字で終わる場合はその文字から始まる単語を入力する
+            小文字で終わる場合はその文字から始まる単語を回答する
             <div>きんぎょ → よーろっぱ</div>
           </li>
           <li className="py-3">
-            伸ばし棒で終わる場合はその前の文字から始まる単語を入力する
+            伸ばし棒で終わる場合はその前の文字から始まる単語を回答する
             <div>すきー → きなこ</div>
           </li>
           <li className="py-3">
@@ -27,7 +27,7 @@ export default function MainPage() {
       </div>
       <Link
         href="/game"
-        className="font-semibold text-xl text-center border mt-12 w-1/4 px-4 py-5 rounded-md shadow-sm text-white bg-indigo-700 hover:bg-indigo-500"
+        className="font-semibold text-xl text-center border mt-12 w-2/5 px-4 py-5 rounded-md shadow-sm text-white bg-indigo-700 hover:bg-indigo-500"
       >
         <div>Play Game</div>
       </Link>
